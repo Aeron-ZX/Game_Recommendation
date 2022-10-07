72da9a7490722cc97d20b696631e8e6cdbce8616
Aeron-ZX <110780250+Aeron-ZX@users.noreply.github.com>
2022-10-07 22:12:39 +0200

    Some formatting for a correct display of text

--- Recomm.py
+++ Recomm.py
@@ -1,21 +1,23 @@
 from gamesData import games, add_game, genres
 from BinarySearchTree import *
 
+#Introduction
 print("""Hi there. You can use this little program to get a recommendation of a videogame. 
  The results are based on a small selection of some of the highest scoring titles of the website Metacritic.
  Metacritic is a review aggregator for entertainment products (films, music, videogames, books...). Each product
  has an average based on critics' scores and another based from users' scores.
  The games selected try to range from different platforms and year of publication. The bare minimum is a score of 80
- points from critics.""")
+ points from critics.
+ \n""")
 
 #This function is used to prompt the user to enter the characters to make the search
 def ask_genre():
-    choice = input("Now then, type the beginning of the genre you'd like to play and let's see what we have ")
+    choice = input("Now then, type the beginning of the genre you'd like to play and let's see what we have\n \n")
     return choice
 
 #This function will prompt the user to confirme the selected genre
 def confirm(choice):
-    confirmation = input(f"The only option with these letters is {choice.title()}. Do you want to look at {choice.title()} games? Press 'y' for yes and 'n' for no ")
+    confirmation = input(f"\nThe only option with these letters is {choice.title()}. Do you want to look at {choice.title()} games? Press 'y' for yes and 'n' for no ")
     while confirmation not in ["y", "n"]:
         confirmation = input("Please, press 'y' or 'n' only ")
     if confirmation == "y":
@@ -30,8 +32,10 @@ def check_genres(choice, genres):
         if genre.startswith(choice):
             found_options.append(genre)
     if found_options == []:
-        choice = """No matching genre has been found with these letters. If you don't know what you are looking for, 
-        some of the most popular genres are 'adventure', 'action' or 'rpg'. Now try again """
+        choice = input("""\nNo matching genre has been found with these letters. If you don't know what you are looking for, 
+some of the most popular genres are 'adventure', 'action' or 'rpg'. Now try again\n 
+\n""")
+        check_genres(choice, genres)
     elif len(found_options) == 1:
         confirmed_choice = confirm(found_options[0])
         if confirmed_choice == "n":
@@ -39,11 +43,12 @@ def check_genres(choice, genres):
         else:
             print_games(confirmed_choice)
     else:
-        print(f"With these beginning letters you may select between these genres: {str(found_options)}")
+        print(f"\nWith these beginning letters you may select between these genres: {str(found_options)}")
         print("\n")
         check_genres(ask_genre(), genres)
 
-
+#This function prints the different games and their attributes, looking through the BST for the genre of choice
+#After that prompts the user to make another search or ending the program
 def print_games(genre):
     games_to_print = BST.get_game_by_genre(genre)
     for game in games_to_print:
@@ -63,7 +68,6 @@ def print_games(genre):
         check_genres(ask_genre(), genres)
     if again == "n":
         print("We hope you enjoy these games!")
-
 
 
 #We use te build-in sort function to order the array by alphabetical order. 
@@ -90,13 +94,22 @@ def sorted_array_to_bst(lst, BST = None):
     right = sorted_array_to_bst(lst[mid:], BST)
     return left and right
 
-
+#We create our BinarySearchTree (BST) with the sorted list of games. 
+#The previous function takes the job of inserting each game into the tree
 BST = sorted_array_to_bst(games)
-print(BST)
+
+
+#This call of the check_genres() function will set all in action to start the search with all user prompts, 
+#make successive searchs or stop the program
+check_genres(ask_genre(), genres)
+
+
+
+
+#The following code has been used for testing or has scarp value. Ignore
+
 #BST.print_tree()
 #print(BST.get_game_by_genre("adventure"))
-
-check_genres(ask_genre(), genres)
 
 # def heapify(lst, BST):
 #     if lst == []:
